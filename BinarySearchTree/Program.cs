9bda3b7a0ba7ce565cb2d6e1b430e644e9cf9cbb
Roqi SB <roqisb9@gmail.com>
2022-12-26 17:16:23 +0700

    create class node

--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -1,1 +1,16 @@
 ﻿using System;
+
+namespace BinarySearchTree
+{
+    class Program
+    {
+
+    }
+    class node
+    {
+        public string info;
+        public node lchild;
+        public node rchild;
+    }
+}
+}
