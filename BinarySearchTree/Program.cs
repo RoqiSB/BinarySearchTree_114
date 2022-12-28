c352aa9360bdc1f4f08c5be08cdc1d1b517d2029
Roqi SB <roqisb9@gmail.com>
2022-12-28 10:49:10 +0700

    create switch case

--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -107,5 +107,32 @@ namespace BinarySearchTree
                 Console.Write(ptr.info + " ");
             }
         }
+        static void Main(string[] args)
+        {
+            BinaryTree x = new BinaryTree();
+            while (true)
+            {
+                Console.WriteLine("\nMenu");
+                Console.WriteLine("1. Implement insert operation");
+                Console.WriteLine("2. Perform inorder traversal");
+                Console.WriteLine("3. Perform preorder traversal");
+                Console.WriteLine("4. Perform postorder traversal");
+                Console.WriteLine("5. Exit");
+                Console.Write("\nEnter your choice (1-5) :");
+                char ch = Convert.ToChar(Console.ReadLine());
+                Console.WriteLine();
+                switch (ch)
+                {
+                    case '1':
+                        {
+                            Console.WriteLine("enter a word: ");
+                            string word = Console.ReadLine();
+                            x.insert(word);
+                        }
+                        break;
+
+                }
+            }
+        }
     }
 }
